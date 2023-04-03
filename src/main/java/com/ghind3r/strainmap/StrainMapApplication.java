c46ea28926ae66f2660e8bdb21d796896841e426
gHind3r <guido.hinderberger@gmail.com>
2023-04-03 18:46:36 +0200

    Initial spring boot service

---
+++ src/main/java/com/ghind3r/strainmap/StrainMapApplication.java
@@ -0,0 +1,13 @@
+package com.ghind3r.strainmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrainMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrainMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghind3r/strainmap/StrainMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghind3r.strainmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrainMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
